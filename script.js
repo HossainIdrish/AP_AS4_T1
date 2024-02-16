1c9f4912b4e6723641971d24844cea0100ab3f54
HossainIdrish <139071336+HossainIdrish@users.noreply.github.com>
2024-02-16 12:44:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ require([
       var view = new MapView({
         container: "viewDiv",
         map: map,
-        center:[-90.1, 38.6],
+        center:[-91.1, 38.6],
         zoom: 9
       });
 
@@ -33,31 +33,23 @@ require([
        **************************************************************/
 
       var template = { // autocasts as new PopupTemplate()
-        title: "Neighborhood: {NHD_NAME}",
+        title: "Neighborhood Name and Number: {NHD_NUMTXT}",
         content: [{
           // It is also possible to set the fieldInfos outside of the content
           // directly in the popupTemplate. If no fieldInfos is specifically set
           // in the content, it defaults to whatever may be set within the popupTemplate.
           type: "fields",
           fieldInfos: [{
-            fieldName: "FID",
-            label: "FID: ",
-            visible: true,
-            format: {
-              digitSeparator: true,
-              places: 0
-            }
-          }, {
-            fieldName: "NHD_NUM ",
-            label: "NHD_NUM: ",
-            visible: true,
-            format: {
-              digitSeparator: true,
-              places: 0
-            }
-          }, {
             fieldName: "NHD_NAME",
-            label: "NHD_NAME",
+            label: "Neighborhood Name: ",
+            visible: true,
+            format: {
+              digitSeparator: true,
+              places: 0
+            }
+          }, {
+            fieldName: "NHD_NUMTXT",
+            label: "Neighborhood Number: ",
             visible: true,
             format: {
               digitSeparator: true,
@@ -67,28 +59,25 @@ require([
         }]
       };
 
-     var symbol = {
-      type: "picture-marker",  // autocasts as new PictureMarkerSymbol()
-      url: "https://graphicriver.img.customer.envatousercontent.com/files/224145068/preview.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=60d4fe57e0a4ead05318f4b9ba91192a",
-      width: "48px",
-      height: "48px"
-};
-  var renderer = {
-      type: "simple",  // autocasts as new SimpleRenderer()
-      symbol: symbol
-    };
-  
-  
       // Reference the popupTemplate instance in the
       // popupTemplate property of FeatureLayer
       var featureLayer = new FeatureLayer({
         url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/ArcGIS/rest/services/STL_Neighborhood/FeatureServer/0",
         outFields: ["*"],
-        popupTemplate: template,
-        renderer:renderer
+        popupTemplate: template
       });
-  
       map.add(featureLayer);
-  
-
+      
+      /*featureLayer.renderer = {
+      type: "simple",  // autocasts as new SimpleRenderer()
+      symbol: {
+        type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+        size: 6,
+        color: "red",
+        outline: {  // autocasts as new SimpleLineSymbol()
+          width: 0.5,
+          color: "white"
+        }
+      }
+    };*/
     });
